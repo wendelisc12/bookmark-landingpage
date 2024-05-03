f00935a58f9aea02ae18ee19a3be634371d3f863
Wendel <wendel.isaac12@gmail.com>
2024-05-03 01:50:05 -0300

    feat: criação das tabs e mostrar conteudo assim que são clicadas

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,71 @@
-$("document").ready(()=>{
-    $(".menu_hamburguer").click(()=>{
+$("document").ready(() => {
+    $(".menu_hamburguer").click(() => {
         $("header").css("display", "none")
         $("header").addClass("header--responsive")
         $("header").slideDown()
     })
 
-    $(".menu_close").click(()=>{
-        $("header").slideUp(()=>{
+    $(".menu_close").click(() => {
+        $("header").slideUp(() => {
             $("header").removeClass("header--responsive")
             $("header").css("display", "block")
         })
+    })
 
+    var linkTabs = $(".features_link")
+    linkTabs.click(e => {
+        linkTabs.toArray().forEach(
+            (tab) => {
+                if ($(tab).hasClass('features_link--active')) {
+                    $(tab).removeClass("features_link--active")
+                }
+            }
+        )
+        $(e.target).addClass("features_link--active")
+
+        if ($(e.target).hasClass("speedy")) {
+            $(".features_selected").html($(
+                `<div class="features_selected--active">
+                    <div>
+                        <img src="images/illustration-features-tab-2.svg" alt="">
+                    </div>
+                    <div class="features_selected_texts">
+                        <h1 class="features_selected_title">intelligent search</h1>
+                        <p class="features_selected_descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis vulputate eros. Nam pellentesque dui id egestas commodo. Nam pellentesque dui id.</p>
+                        <button class="features_selected_button">More info</button>
+                    </div>
+                </div>`
+            ).css("display", "none").fadeIn())
+        }else if($(e.target).hasClass("easy")){
+            $(".features_selected").html($(
+                `
+                <div class="features_selected--active">
+                    <div>
+                        <img src="images/illustration-features-tab-3.svg" alt="">
+                    </div>
+                    <div class="features_selected_texts">
+                        <h1 class="features_selected_title">share your bookmarks    </h1>
+                        <p class="features_selected_descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis vulputate eros. Nam pellentesque dui id egestas commodo. Nam pellentesque dui id.</p>
+                        <button class="features_selected_button">More info</button>
+                    </div>
+                </div>
+                `
+            ).css("display", "none").fadeIn())
+        }else if($(e.target).hasClass("simple")){
+            $(".features_selected").html($(
+                `
+                <div class="features_selected--active">
+                    <div>
+                        <img src="images/illustration-features-tab-1.svg" alt="">
+                    </div>
+                    <div class="features_selected_texts">
+                        <h1 class="features_selected_title">bookmark in one click</h1>
+                        <p class="features_selected_descricao">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis vulputate eros. Nam pellentesque dui id egestas commodo. Nam pellentesque dui id.</p>
+                        <button class="features_selected_button">More info</button>
+                    </div>
+                </div>
+                `
+            ).css("display", "none").fadeIn())
+        }
     })
 })
